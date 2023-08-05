7a0662224ddf1ed26d68c2a36365c543cd955225
Robert Shepherd <rshepherdcpp@gmail.com>
2023-08-05 16:30:30 +0100

    fix vector out of range error

--- Board.cpp
+++ Board.cpp
@@ -201,7 +201,7 @@ auto Board::RandomNumber() -> int
 {
     std::random_device dev;
     std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist6(0, 9); // distribution in range [1, 10]
+    std::uniform_int_distribution<std::mt19937::result_type> dist6(0, 8); // distribution in range [1, 10]
 
     return dist6(rng);
 }
